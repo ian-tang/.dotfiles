2430cd683ae5e4bf1e69bf20e02466dfba16904b
Ian Tang <ian.tang@protonmail.com>
2025-03-01 23:00:12 -0500

    auto commit: Sat Mar  1 23:00:12 EST 2025

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -779,6 +779,9 @@ require("lazy").setup({
 				-- You can use a sub-list to tell conform to run *until* a formatter
 				-- is found.
 				javascript = { { "prettierd", "prettier" } },
+				javascriptreact = { { "prettierd", "prettier" } },
+				typescript = { { "prettierd", "prettier" } },
+				typescriptreact = { { "prettierd", "prettier" } },
 				css = { { "prettierd", "prettier" } },
 				c = { "clang-format" },
 			},
